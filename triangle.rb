711055d84e797c29d32bfc5e3965277ce40a1788
Amy Unger <amy.e.unger@gmail.com>
2012-08-29 20:49:42 -0500

    complete triangle 2

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,7 @@
 #
 def triangle(a, b, c)
   sides = [a,b,c]
+  validate_sides(sides)
 
   unique_side_lengths = sides.uniq.length
 
@@ -26,6 +27,28 @@ def triangle(a, b, c)
   when 3
     :scalene
   end
+end
+
+def validate_sides(sides)
+  # Check if any sides are 0 or negative
+  sides.each do |s|
+    if s <= 0
+      raise TriangleError, 'Invalid Triangle side length'
+    end
+  end
+
+  # Ensure that they could form a triangle  
+  unless sides_can_form_triangle(sides)
+    raise TriangleError, 'Invalid Triangle sides: cannot form triangle' 
+  end
+end
+
+def sides_can_form_triangle(sides)
+
+  a, b, max = sides.sort
+
+  max < a + b
+
 end
 
 # Error class used in part 2.  No need to change this code.
